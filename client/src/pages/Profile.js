671461171ae837223077368985b7ee67fca0a07e
Carmen Chow <carmendevelops@gmail.com>
2023-03-06 00:44:23 -0500

    profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import './Profile.css'
+import { BsPencil } from 'react-icons/bs'
 
 const Profile = () => {
   return (
@@ -8,15 +9,20 @@ const Profile = () => {
       <Navbar/>
       <div className="user-profile">
       <h1>My Profile</h1>
+      <div className="my-profile">
+        <p className="name"></p>
+        <button className="edit"><BsPencil/>Edit Profile</button>
+      </div>
 
-        <div className="events">Your events</div>
-        <div className="events">Your donations</div>
-        <div className="events">Your volunteer hours</div>
+
+        <div className="events">Events</div>
+        <div className="events">Donations</div>
+        <div className="events">Volunteer hours</div>
 
         <div className="favourites">
           <h3>Favourited Organizations</h3>
-          
         </div>
+        
       </div>
     </section>
   )
