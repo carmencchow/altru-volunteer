6d9a962208f98a545b32f8e3db35492080f397be
Carmen Chow <carmendevelops@gmail.com>
2023-03-10 19:49:26 -0500

    generate JWT and return token to use when login and register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,37 +8,45 @@ const { countDocuments } = require('../models/userModel');
 
 // 1. REGISTER endpoint
 const signup = async (req, res) => {
+  const { username, email, password } = req.body;
   try {
-    // Validate incoming request
-    if (!req.body.username || !req.body.password || !req.body.email){
-      res.status(404).send({ message: 'Please complete all required fields'});
+    // a) Validate incoming request
+    if (!username || !password || !email){
+      res.status(400).send({ message: 'Please complete all required fields'});
       return;
     }
 
-    // Hash the password before saving email and password into the database
-    const hashedPassword = await bcrypt.hash(req.body.password, 8);
-
-    // Create a new user instance and collect the data
-    const newUser = new User(req.body) 
-    // const newUser = new User({
-    //   username: req.body.username,
-    //   email: req.body.email,
-      // password: hashedPassword,
-    //   image: req.body.image
-    // });
-
-    // Check if a user with the same username already exists
-    const existingUser = await User.findOne({ username: req.body.username.toLowerCase()});
-    if (existingUser){
-      res.status(404).send({ message: 'User name already exists, please enter a different username'});
+    // b) Check if user already exists
+    const userExists = await User.findOne({ email});
+    if (userExists){
+      res.status(400).send({ message: 'Email already exists. Please login instead.'});
     } 
 
+    // c) Hash the password before saving email and password into the database
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // d) Create a new user instance and collect the data
+    const newUser = new User(req.body) 
+  // const newUser = new User({
+      // username: req.body.username,
+      // email: req.body.email,
+      // password: hashedPassword,
+      // image: req.body.image
+    // })
+
+    // e) Save user and return data to user
     await newUser.save();
-    res.status(200).send({ message: 'New user saved to database' }); 
+
+    res.status(200).send({ 
+      message: 'New user saved to database', 
+      username, 
+      email, 
+      token: createToken(newUser._id) 
+    }); 
   
     } catch (err) {
       console.log(err);
-      res.status(500).send({ message: 'Error creating user' });
+      res.status(500).send({ message: 'Invalid user data. User not saved' });
     };
   };
 
@@ -49,44 +57,63 @@ const signup = async (req, res) => {
 
     try {
       // Validate incoming request body
-      if (!req.body.username || !req.body.password){
-        res.status(400).send({ message: 'Username or password missing'});
+      if (!req.body.email || !req.body.password){
+        res.status(400).send({ message: 'Email or password missing'});
+        return;
       }
 
-      // Find user with the given username
-      const user = await User.findOne({ username: req.body.username.toLowerCase() });
-      if (!user) return res.status(404).send({ message: 'User not found' });
+      // Check for user email
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) return res.status(404).send({ message: 'User not found. Please enter the correct email and password' });
 
       // Validate incoming password against hashed password
       const isPasswordValid = await bcrypt.compare(req.body.password, hashedPassword)
-      console.log(req.body.password, hashedPassword);
-        
+ 
       if (!isPasswordValid){
         res.status(400).send({ message: 'Invalid username or password', err});
       }
 
-      // Create a JWT token for the authenticated user
-      const token = jwt.sign({
-        "id": user._id,
-        "username": user.username,
-        "email": user.email
-      }, process.env.JWT_SECRET, { expiresIn: '20s' }) 
+      res.status(200).send({ 
+        message: 'User login successful', 
+        username: req.body.username, 
+        email: req.body.email,
+        token: createToken(user._id) 
+      }); 
 
-      // Create cookie with token
-      res.cookie('jwt', token, {
-        httpOnly: true,
-        secure: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000
-      })
-
-      // Send accessToken containing username 
-      res.status(200).send({ message: 'Sending TOKEN', data: token });
-      
     } catch (err) {
       console.log(err); 
       res.status(500).send({ message: 'Login failed', err});
     }
   };
+
+
+  // Generate JWT token
+  const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.JWT_SECRET, { 
+      expiresIn: '7d',
+    })
+  }
+        // res.status(200).send({ 
+        //   message: 'Login successful', 
+        //   username, 
+        //   email, 
+        //   token: createToken(newUser._id) 
+        // }); 
+      // }
+    
+      // // Create cookie with token
+      // res.cookie('jwt', token, {
+      //   httpOnly: true,
+      //   secure: true,
+      //   maxAge: 7 * 24 * 60 * 60 * 1000
+      // })
+
+      
+  //   } catch (err) {
+  //     console.log(err); 
+  //     res.status(500).send({ message: 'Login failed', err});
+  //   }
+  // };
 
 // 3. LOGOUT endpoint
 const logout = ( req, res ) => {
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -3,7 +3,6 @@ const { signup, login, logout } = require ('../controllers/authController')
 
 const router = express.Router();
 
-// signup, login, and logout routes
 router.post('/signup', signup)  // localhost:5000/api/auth/signup
 router.post('/login', login) // localhost:5000/api/auth/login
 router.post('/logout', logout) // localhost:5000/api/auth/logout
