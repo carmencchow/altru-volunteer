b8aa74008290b30f96d3d551bd6904421d196662
Carmen Chow <carmendevelops@gmail.com>
2023-05-01 09:20:16 -0400

    update key

--- client/src/components/StripeButton.js
+++ client/src/components/StripeButton.js
@@ -2,32 +2,27 @@ import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 const stripeBtn = () => {
-  const publishableKey = 
-  // process.env.REACT_APP_STRIPE_KEY
-"pk_test_51L1kSgAoNhpouPlcggVUZyCuhwjZKomWM4sK8IrNj9OI3OumyeYeNkOrSPTrshrj8vbjJdA82r7FBgKueeUOzNbk00vJ7IMMjT"
-
-// REACT_APP_STRIPE_KEY = 
+  const publishableKey =
+    "pk_test_51L1kSgAoNhpouPlcjJkJ9Fh5Bi2bSrccKhv0rQuL5zbG70ttMTqiaTfCj46FpF9t2PHXECrUBD26IZIfWAJYcrzs00UCsQLPDr";
 
   // Send card info to Stripe and returns a token object
-  const onToken = token => {
+  const onToken = (token) => {
     const body = {
       amount: 999,
-      token: token
-  };
-  axios
+      token: token,
+    };
+    axios
       // Send token and amount to the backend in the body with axios request
       .post("http://localhost:8000/payment", body)
-      .then(response => {
+      .then((response) => {
         console.log(response);
         alert("Payment Success");
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Payment Error: ", error);
         alert("Payment Error");
       });
   };
-  return (
-    <StripeCheckout/>
-  );
+  return <StripeCheckout />;
 };
 export default stripeBtn;
