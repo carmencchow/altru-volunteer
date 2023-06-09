537a1ba942e4ca0ee3e7e2d527d0db23f8359ff4
cchow33 <109078860+cchow33@users.noreply.github.com>
2023-06-09 16:37:23 -0400

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://altru-volunteer-be.onrender.com/api/",
-  baseURL: "http://localhost:5000/api/",
+   baseURL: "https://altru-volunteer-be.onrender.com/api/",
+  //baseURL: "http://localhost:5000/api/",
   headers: {
     "Content-Type": "application/json",
   },
