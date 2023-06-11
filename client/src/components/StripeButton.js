d910f937c7e6b4d8d5734e84696b3c423a4a90c2
Carmen Chow <carmendevelops@gmail.com>
2023-06-11 14:25:11 -0400

    switch deploy url

--- client/src/components/StripeButton.js
+++ client/src/components/StripeButton.js
@@ -1,18 +1,22 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
+import { api } from "../utils/axios";
+
 const stripeBtn = () => {
   const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
 
   // Send card info to Stripe and returns a token object
-  const onToken = (token) => {
+  const onToken = async (token) => {
     const body = {
       amount: 999,
       token: token,
     };
-    axios
+
+    await api
       // Send token and amount to the backend in the body with axios request
-      .post("http://localhost:5000/payment", body)
+      // .post("http://localhost:5000/payment", body)
+      .post("/payment", body)
       .then((response) => {
         console.log(response);
         alert("Payment Success");
