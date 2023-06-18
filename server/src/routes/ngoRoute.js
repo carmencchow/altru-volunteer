4a8549686e5fff3f368a5d0e7acb8d884b094555
cchow33 <109078860+cchow33@users.noreply.github.com>
2023-06-17 20:22:33 -0400

    decrease volunteer count

--- server/src/routes/ngoRoute.js
+++ server/src/routes/ngoRoute.js
@@ -10,6 +10,7 @@ const router = express.Router();
 
 router.get("/", getNgos);
 router.get("/:id", getNgo);
+router.put("/:id/decrement", updateNgo);
 router.get("/:frequency/:category", getFiltered);
 router.post("/", createNgo);
 
